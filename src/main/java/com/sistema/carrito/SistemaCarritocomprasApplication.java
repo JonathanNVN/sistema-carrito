3fd363331f671c9810084680b5a55e5993620cfb
JonathanNVN <user.jn2@mail.com>
2023-07-18 15:01:14 -0500

    creación de la estructura del proyecto y de las entidades

---
+++ src/main/java/com/sistema/carrito/SistemaCarritocomprasApplication.java
@@ -0,0 +1,14 @@
+package com.sistema.carrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class SistemaCarritocomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCarritocomprasApplication.class, args);
+	}
+
+}
