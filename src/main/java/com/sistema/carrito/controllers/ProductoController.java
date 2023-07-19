a91c074f0e25274351c80a28615f05c92c3e6569
JonathanNVN <user.jn2@mail.com>
2023-07-19 11:59:02 -0500

    Creando la funcionalidad 'Guardar producto', 'Vista Create' y 'Logger' para la clase 'Producto Controller'

--- src/main/java/com/sistema/carrito/controllers/ProductoController.java
+++ src/main/java/com/sistema/carrito/controllers/ProductoController.java
@@ -1,15 +1,48 @@
 package com.sistema.carrito.controllers;
 
+
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sistema.carrito.models.Producto;
+import com.sistema.carrito.models.Usuario;
+import com.sistema.carrito.service.ProductoService;
+
+
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER = (Logger) LoggerFactory.getLogger(ProductoController.class);
 
+	@Autowired
+	ProductoService productoService;
+	
 	@GetMapping("")
 	public String show() {
 		return "productos/show";
+	}
+	
+	@GetMapping("/create")
+	public String create(){
+		return "/productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String save(Producto producto){
+		
+		LOGGER.info("Este es el objeto producto {}", producto);
+		
+		Usuario usuario = new Usuario(1,"","","","","","","");
+		producto.setUsuario(usuario);		
+		
+		productoService.save(producto);	
+		
+		return "redirect:/productos";
 	}
 }
