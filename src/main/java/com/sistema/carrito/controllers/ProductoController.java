02442b115a6deeafc1166fabd538d0ba4da34ee8
JonathanNVN <user.jn2@mail.com>
2023-07-19 12:55:27 -0500

    Funcionalidad para mostrar los productos para el administrador

--- src/main/java/com/sistema/carrito/controllers/ProductoController.java
+++ src/main/java/com/sistema/carrito/controllers/ProductoController.java
@@ -4,6 +4,7 @@ package com.sistema.carrito.controllers;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,9 @@ public class ProductoController {
 	ProductoService productoService;
 	
 	@GetMapping("")
-	public String show() {
+	public String show(Model model) {
+		
+		model.addAttribute("productos", productoService.findAll());
 		return "productos/show";
 	}
 	
