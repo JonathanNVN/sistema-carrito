ffea21ead7807eeb3fb5403cfb3c30b1e83520f5
JonathanNVN <user.jn2@mail.com>
2023-07-22 17:16:34 -0500

    Metodo para generar un numero en la orden

--- src/main/java/com/sistema/carrito/service/IOrdenService.java
+++ src/main/java/com/sistema/carrito/service/IOrdenService.java
@@ -1,8 +1,12 @@
 package com.sistema.carrito.service;
 
+import java.util.List;
+
 import com.sistema.carrito.models.Orden;
 
 public interface IOrdenService {
+	
+	List<Orden> findAll();
 	
 	Orden save(Orden orden);
 
--- src/main/java/com/sistema/carrito/service/OrdenServiceImplement.java
+++ src/main/java/com/sistema/carrito/service/OrdenServiceImplement.java
@@ -1,5 +1,8 @@
 package com.sistema.carrito.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,42 @@ public class OrdenServiceImplement implements IOrdenService{
 	@Override
 	public Orden save(Orden orden) {
 		return iOrdenRepository.save(orden);
+	}
+
+	@Override
+	public List<Orden> findAll() {
+		return iOrdenRepository.findAll();
+	}
+	
+	public String generarNumeroOrden () {
+		
+		int numero = 0;
+		String numConcat = "";
+		
+		List<Orden> ordenes = findAll();
+		
+		List<Integer> numeros = new ArrayList<Integer>();
+		
+		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+		
+			if(ordenes.isEmpty()) {
+				numero = 1;
+			}else {
+				numero = numeros.stream().max(Integer::compare).get();
+				numero++;
+			}
+			
+			if(numero < 10) {
+				numConcat = "000000000" + String.valueOf(numero);
+			}else if(numero < 100) {
+				numConcat = "00000000" + String.valueOf(numero);
+			}else if (numero < 1000) {
+				numConcat = "00000000" + String.valueOf(numero);
+			}else if (numero < 10000) {
+				numConcat = "00000000" + String.valueOf(numero);
+			} //...
+		
+		return numConcat;
 	}
 
 }
